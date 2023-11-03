dddaaffe9c2f683b8d548bd73710f51b3fc62605
Hedfi Nourchen <nourchen.hedfi@esprit.tn>
2023-11-03 22:53:56 +0100

    update

--- src/app/shared/Service/Stock.service.ts
+++ src/app/shared/Service/Stock.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 
 import { HttpClient} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class StockService {
-  readonly API_URL = 'http://localhost:8089/SpringMVC/stock';
+  
+  readonly API_URL = environment.API_Base;
 
   constructor(private httpClient: HttpClient) { }
 
